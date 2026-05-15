e9e1d123be54a27b4b5f20ca37828788ce7335cb
Johnny Pham <johnnypham14112003@gmail.com>
2026-05-15 12:18:26 +0700

    refactor: add template

---
+++ ExampleUmbraco/ExampleUmbraco/Controllers/ShopPageController.cs
@@ -0,0 +1,26 @@
+﻿using Service.Interfaces;
+using System;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace ExampleUmbraco.Controllers
+{
+    public class ShopPageController : RenderMvcController
+    {
+        private readonly IMotorService _motorService;
+
+        public ShopPageController(IMotorService motorService)
+        {
+            _motorService = motorService;
+        }
+
+        public override ActionResult Index(RenderModel model)
+        {
+                var motorList = _motorService.GetAll();
+                ViewBag.MotorList = motorList;
+
+                return CurrentTemplate(model);
+        }
+    }
+}
