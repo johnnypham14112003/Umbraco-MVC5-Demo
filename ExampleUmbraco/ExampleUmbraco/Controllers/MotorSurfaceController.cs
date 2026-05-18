0c6c749ea35908745b7c9f46bac3799397472bfb
Johnny Pham <johnnypham14112003@gmail.com>
2026-05-18 16:13:03 +0700

    feat: add CRUD action

---
+++ ExampleUmbraco/ExampleUmbraco/Controllers/MotorSurfaceController.cs
@@ -0,0 +1,52 @@
+﻿using ExampleUmbraco.App_Start;
+using Service.Interfaces;
+using Service.ViewModels;
+using System;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace ExampleUmbraco.Controllers
+{
+    public class MotorSurfaceController : SurfaceController
+    {
+        private readonly IMotorService _motorService;
+
+        public MotorSurfaceController()
+        {
+            _motorService = AppServiceLocator.GetMotorService();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddMotor(MotorViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = Guid.NewGuid();
+                _motorService.Add(model);
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateMotor(MotorViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _motorService.Update(model);
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteMotor(Guid id)
+        {
+            _motorService.Delete(id);
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
